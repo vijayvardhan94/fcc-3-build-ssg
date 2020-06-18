829a634aaf0092428cd591815a833d9baa3e9cdf
Vijay Tadimeti <tadimetv@oregonstate.edu>
2020-06-17 22:01:01 -0700

    keys

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-require('dotenv').config();
+require('dotenv').config()
 
 module.exports = async function(){
     try{
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
         return response.data;
         console.log(response);
     }catch(error){
