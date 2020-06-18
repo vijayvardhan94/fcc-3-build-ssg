a1a6f6a77ec01164f29ef6d9fa3b1ebb8497de7d
Vijay Tadimeti <tadimetv@oregonstate.edu>
2020-06-17 21:56:34 -0700

    api calls

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+
+//509ad6dc159d4794b40dc22a3f92be19
+  
 module.exports = function(config){
     config.addPassthroughCopy("src/js");
     return {
---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async function(){
+    try{
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+        return response.data;
+        console.log(response);
+    }catch(error){
+        console.log(error);
+    }
+};
+//509ad6dc159d4794b40dc22a3f92be19
